c2212adab1e4a428a2ec2da47439594b0e520527
Victor Ajagunna <victor-ajagunna@hotmail.com>
2023-02-16 20:40:29 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Homepage from './components/homepage/Homepage';
+import Cart from './pages/cart/Cart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
